58f51fd8dffe983eda5e1af567e87d7e54d49009
herman888 <341140944@gapps.yrdsb.ca>
2024-04-29 21:39:00 -0400

    Game Over When Apple Falls

--- MyWorld.java
+++ MyWorld.java
@@ -18,7 +18,7 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1);
+        super(600, 400, 1, false);
         
         Elephant elephant = new Elephant();
         addObject(elephant, 300, 200);
@@ -29,6 +29,16 @@ public class MyWorld extends World
         createApple();
         
     }
+    
+    public void gameOver()
+    {
+        Label gameOverLabel = new Label("Game Over", 100);
+        addObject(gameOverLabel, 300, 200);
+    }
+    
+    
+    
+    
     
     public void increaseScore()
     

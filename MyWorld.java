84fab56b5ef12eb8361130694c221b25c0cfe231
herman888 <341140944@gapps.yrdsb.ca>
2024-04-29 21:00:37 -0400

    Add Elephant and Make Him Move

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
+ * The World
  * 
  * @author (your name) 
  * @version (a version number or a date)

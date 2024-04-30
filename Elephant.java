9ffb1faadb000eb7349cddfa61c91a27a58a3a74
herman888 <341140944@gapps.yrdsb.ca>
2024-04-29 21:10:03 -0400

    Make Elephant Move based on Keyboard

--- Elephant.java
+++ Elephant.java
@@ -14,6 +14,13 @@ public class Elephant extends Actor
      */
     public void act()
     {
-        
+        if(Greenfoot.isKeyDown("left"))
+        {
+            move(-1);
+        }
+        else if(Greenfoot.isKeyDown("right"))
+        {
+            move(1);
+        }
     }
 }

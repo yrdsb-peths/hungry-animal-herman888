cf62a5bd31664083d994a47f0e968192ea120729
herman888 <341140944@gapps.yrdsb.ca>
2024-05-08 17:48:07 -0400

    Add animations to elephant sprite

--- Elephant.java
+++ Elephant.java
@@ -8,14 +8,23 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Elephant extends Actor
 {
+    GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
+    GreenfootImage[] idle = new GreenfootImage[8];
     public Elephant()
     {
-        setImage(idle);
+        for(int i = 0; i < 7; i++)
+        {
+            idle[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+        }
+        setImage(idle[0]);
     }
-    
-    
-    GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
-    GreenfootImage idle = new GreenfootImage("images/elephant_idle/idle0.png");
+    int imageIndex = 0;
+    public void animateElephant()
+    {
+        setImage(idle[imageIndex]);
+        imageIndex = (imageIndex + 1) % idle.length;
+    }
+  
     /**
      * Act - do whatever the Elephant wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -32,6 +41,8 @@ public class Elephant extends Actor
         }
         
         eat();
+        
+        animateElephant();
        
     }
     

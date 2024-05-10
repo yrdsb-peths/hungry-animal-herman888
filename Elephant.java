c4afc4370b28cc748be7cfe68364b4c0bc0ca87f
herman888 <341140944@gapps.yrdsb.ca>
2024-05-10 19:41:04 -0400

    Making a timer for elephants tail

--- Elephant.java
+++ Elephant.java
@@ -15,6 +15,8 @@ public class Elephant extends Actor
     
     
     String facing = "right";
+    
+    SimpleTimer animationTimer = new SimpleTimer();
     public Elephant()
     {
         for(int i = 0; i < idleRight.length; i++)
@@ -28,11 +30,17 @@ public class Elephant extends Actor
             idleLeft[i].mirrorHorizontally();
             idleLeft[i].scale(100,100);
         }
+        animationTimer.mark();
         setImage(idleRight[0]);
     }
     int imageIndex = 0;
     public void animateElephant()
     {
+        if(animationTimer.millisElapsed() < 300)
+        {
+            return;
+        }
+        animationTimer.mark();
         if(facing.equals("right"))
         {
             setImage(idleRight[imageIndex]);

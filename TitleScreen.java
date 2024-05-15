cac5be20b6813b6d35fc571bdc41036c48539d45
herman888 <341140944@gapps.yrdsb.ca>
2024-05-15 18:45:41 -0400

    Update Fixed Title Screen

--- TitleScreen.java
+++ TitleScreen.java
@@ -1,15 +1,14 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * title Screen
+ * Write a description of class TitleScreen here.
  * 
- * @author (Herman isayenka) 
+ * @author (your name) 
  * @version (a version number or a date)
  */
 public class TitleScreen extends World
 {
-    Label titleLabel = new Label("Hungary Elephant", 60);
-
+    Label titleLabel = new Label("Hungry Elephant", 60);
     /**
      * Constructor for objects of class TitleScreen.
      * 
@@ -24,10 +23,11 @@ public class TitleScreen extends World
     
     public void act()
     {
-        if(Greenfoot.isKeyDown("space"));
+        if(Greenfoot.isKeyDown("space"))
         {
             MyWorld gameWorld = new MyWorld();
             Greenfoot.setWorld(gameWorld);
         }
+        
     }
 }
